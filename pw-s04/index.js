a4325d4c89fad8b16c869ba699adfd39df7d7f10
unknown <ramosalvaro1001@gmail.com>
2022-05-31 11:18:14 -0500

    Carga de datos

---
+++ pw-s04/index.js
@@ -0,0 +1,36 @@
+function square(n){
+  return n*n;
+}
+
+//Arrow Notation
+const cube = x =>{
+  console.log("en función de cube...");
+  return x*x*x;
+}
+
+function greet(){
+  return "Hola Mundo";
+}
+
+function showResult(result){
+  //Obtenemos una referencia al elemento HTML ocn ese ID
+  //Podemos modificar sus atributos 
+  document.getElementById("txtResult").value = result;
+}
+
+const sum = (a,b) => a + b;
+
+// Indica el tipo de triangulo que es
+const getTrianguleType  = (a,b,c) =>{
+  if(a == b && a == c){
+    return "Equilatero";
+  }else if(a == b || a == c || b == c){
+    return "Isosceles";
+  }else{
+    return "Escaleno";
+  }
+}
+
+const student = {firstName: "Alvaro", lastName: "Ramos", age: 15};
+
+const legalAge = student => student.age >= 18 ? "Mayor de edad" : "Menor de edad";

8920c3265ed5f5ad891a5fc9d0d9e250269bf10b
unknown <ramosalvaro@gmail.com>
2022-05-31 19:02:14 -0500

    actualizacion

---
+++ pw-s04/calculadora.js
@@ -0,0 +1,7 @@
+function getProduct(){
+  value1 = parseInt(document.getElementById("txtValue1").value);
+  value2 = parseInt(document.getElementById("txtValue2").value);
+  document.getElementById("lblResult").innerHTML = value1 + value2;
+  document.getElementById("divTitle").innerHTML = "<h3>Titulo Cambiado</h3>";
+  document.getElementById("btnMultiply")
+}
